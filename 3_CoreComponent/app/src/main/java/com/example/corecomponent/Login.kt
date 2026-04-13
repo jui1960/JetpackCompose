0e5b8b469e4d3f2c80784d9c8f8a0e95404bdd22
jui1960 <juifull196044@gmail.com>
2026-04-13 08:23:46 +0600

    add

--- 3_CoreComponent/app/src/main/java/com/example/corecomponent/Login.kt
+++ 3_CoreComponent/app/src/main/java/com/example/corecomponent/Login.kt
@@ -29,7 +29,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview(showSystemUi = true)
+//@Preview(showSystemUi = true)
 @Composable
 fun LoginPage() {
     val context = LocalContext.current
